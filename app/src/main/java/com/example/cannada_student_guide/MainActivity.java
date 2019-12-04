2624b282edf7b4deeacd5de431ae8d5933e6e08b
Kashyap Kokkiligadda <A00209673@mycambrian.ca>
2019-12-04 10:32:15 -0500

    changes for locale

--- app/src/main/java/com/example/cannada_student_guide/MainActivity.java
+++ app/src/main/java/com/example/cannada_student_guide/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.cannada_student_guide;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.canada_student_guide.util.LocaleHelper;
+
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     ScrollView guide1Body,guide2Body;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +47,31 @@ public class MainActivity extends AppCompatActivity {
                 ));
                 return true;
             case R.id.menu_about:
+                Log.e("menu_about: ", "menu_about");
                 Toast.makeText(this, R.string.about_summary, Toast.LENGTH_LONG).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.e("pref_languages: ", "pref_languages");
+        switch(key)
+        {
+            case "pref_languages":
+                // DO STUFF
+                Log.e("pref_languages: ", "pref_languages");
+                LocaleHelper.setLocale(MainActivity.this, "fr");
+
+                //It is required to recreate the activity to reflect the change in UI.
+                recreate();
+                break;
+
+            case "KEY2":
+                // DO STUFF
+                break;
         }
     }
 
--- app/src/main/java/com/example/cannada_student_guide/SettingsFragment.java
+++ app/src/main/java/com/example/cannada_student_guide/SettingsFragment.java
@@ -9,11 +9,13 @@ import android.os.Bundle;
 
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.example.canada_student_guide.util.LocaleHelper;
 
 
 
-
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
 
     private SharedPreferences prefs;
@@ -27,5 +29,25 @@ public class SettingsFragment extends PreferenceFragment {
         // which did a lot more work for us
         addPreferencesFromResource(R.xml.preferences);
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Log.e("pref_languages: ", "pref_languages");
+        switch(key)
+        {
+            case "pref_languages":
+                // DO STUFF
+                Log.e("pref_languages: ", "pref_languages");
+                LocaleHelper.setLocale(MainActivity.this, "fr");
+
+                //It is required to recreate the activity to reflect the change in UI.
+                recreate();
+                break;
+
+            case "KEY2":
+                // DO STUFF
+                break;
+        }
     }
 }
